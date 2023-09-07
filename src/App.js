5c8b11d09ea66df1f488a6e79f625fa0112f3dcd
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-09-07 12:52:11 +0500

    add Expense component to split App Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expense from "./components/Expense";
 import "./App.css";
 
 function App() {
@@ -23,30 +23,7 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-    </div>
-  );
+  return <Expense expenses={expenses}></Expense>;
 }
 
 export default App;
