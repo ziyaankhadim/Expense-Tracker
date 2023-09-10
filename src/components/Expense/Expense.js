a63e219842ccdbfda2dca4ae3e33a128bc4f6b71
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-09-10 17:48:58 +0500

    add key to Expense component because each child in list should have unique key prop

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -17,6 +17,7 @@ function Expense(props) {
       />
       {props.items.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
