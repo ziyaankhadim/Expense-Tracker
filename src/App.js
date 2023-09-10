8516cf4e7ce3a70c9ab51c797511761369f74a43
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-09-10 17:08:44 +0500

    pass the expense data from NewExpense(child) to App(parent)

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expense expenses={expenses}></Expense>
     </div>
   );
--- src/components/New Expense Form/NewExpense.js
+++ src/components/New Expense Form/NewExpense.js
@@ -1,13 +1,13 @@
 import "./NewExpense.css";
 
 import ExpenseForm from "./ExpenseForm";
-function NewExpense() {
+function NewExpense(props) {
   const SaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    props.onAddExpense(expenseData);
   };
 
   return (
